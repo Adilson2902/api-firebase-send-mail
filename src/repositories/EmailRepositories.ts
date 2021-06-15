37779adc409b983470a0f6e741de3b3e9b0916cc
Adilson2902 <38479849+Adilson2902@users.noreply.github.com>
2021-06-15 15:10:55 -0300

    Update EmailRepositories.ts

--- src/repositories/EmailRepositories.ts
+++ src/repositories/EmailRepositories.ts
@@ -10,9 +10,8 @@ export class EmailFuncionts{
 
 
 async sendEmail(to:string,message:string,subject:string){
-    const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID,process.env.CLIENT_SECRET,process.env.REDIRECT_URI);
-   
     
+  const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID,process.env.CLIENT_SECRET,process.env.REDIRECT_URI);  
 
     oAuth2Client.setCredentials({refresh_token:process.env.REFRESH_TOKEN});
 
